bf0465243f42b0a24666a1d335444f4072943603
aolinyang <allenyang2813@gmail.com>
2022-02-09 06:39:16 -0600

    added menu js

---
+++ load_menu.js
@@ -0,0 +1,14 @@
+
+
+    document.addEventListener("DOMContentLoaded", function(event) { 
+        document.getElementById("menu").innerHTML = `
+    <menu>
+        <a href="research.html">Research</a>
+        <a href="resources.html">Resources</a>
+        <a href="spacenews.html">Space News</a>
+        <a href="prevspeakers.html">Previous Speakers</a>
+        <a href="about.html">About</a>
+        <a href="history.html">History</a>
+        <a href="current.html">Current Events/Newsletter</a>
+    </menu>`
+      });
